6d8a9842c2f6b376cc64b0175ce5b3e12e105c19
David Klimavich <dklimavich@gmail.com>
2023-04-18 13:07:06 -0400

    Added more packages and fixed some incorrect names

--- win_init.ps1
+++ win_init.ps1
@@ -16,6 +16,7 @@ scoop bucket add games
 scoop bucket add versions
 scoop bucket add nonportable
 scoop bucket add main
+
 scoop update
 choco upgrade chocolatey
 
@@ -24,6 +25,11 @@ if($all -Or $basicapps)
     scoop install firefox
     scoop install powertoys
     choco install libreoffice-fresh
+}
+if($all -Or $mediaapps)
+{
+    scoop install gimp
+    scoop install audacity
 }
 if($all -Or $gameapps)
 {
@@ -45,16 +51,11 @@ if($all -Or $fileutils)
 }
 if($all -Or $devtools)
 {
-    scoop install ctags
+    scoop install universal-ctags
     scoop install gcc
     scoop install git
     scoop install python
     scoop install vscode
     scoop install portable-virtualbox 
     scoop install windows-terminal
-}
-
-if($all)
-{
-    scoop install musescore
 }
